38d5f82c7f4f3047eeff1a43d18dfa2b6c3ad178
kaitlynzou <kaitlyn.zou@gmail.com>
2023-09-28 09:46:02 -0400

    Week03 Randomize APP

---
+++ Week03/Week03Ramdomize/Week03Ramdomize/ContentView.swift
@@ -0,0 +1,81 @@
+//
+//  ContentView.swift
+//  Week03Ramdomize
+//
+//  Created by Kaitlyn Zou on 9/28/23.
+//
+
+import SwiftUI
+
+var images = ["rectangle",
+              "triangle",
+              "hexagon",
+              "pentagon",
+              "rhombus",
+              "diamond",
+              "circle",
+              "seal",
+              "oval",
+              "capsule"]
+
+struct ContentView: View {
+
+// allows us to modify values inside a struct
+@State var random: String = ""
+@State var imagePosition = CGPoint(x: 100, y: 100)
+@State var imageSize = CGSize(width: 100, height: 100)
+let screenSize = UIScreen.main.bounds.size
+
+var body: some View {
+    VStack {
+        Image(systemName: random)
+            .resizable()
+            .frame(width: imageSize.width, height: imageSize.height)
+            .position(imagePosition)
+            .foregroundColor(Color(red: Double.random(in: 0...255) / 255, green: Double.random(in: 0...255) / 255, blue: Double.random(in: 0...255) / 255))
+
+        Button(action: {
+            self.random = chooseRandomImage()
+            self.randomizeImagePosition()
+            self.randomizeImageSize()
+
+        }) {
+            Text("Randomize!")
+        }
+            .font(.title3)
+            .padding()
+            .background(Color.pink)
+            .foregroundColor(.white)
+            .cornerRadius(10)
+    }
+    .background(Color(red: Double.random(in: 0...255) / 255, green: Double.random(in: 0...255) / 255, blue: Double.random(in: 100...200) / 255)
+)
+
+}
+    func randomizeImagePosition() {
+        let randomX = CGFloat.random(in: 0...(screenSize.width - 100)) // Adjust 100 for image width
+        let randomY = CGFloat.random(in: 0...(screenSize.height - 100)) // Adjust 100 for image height
+        self.imagePosition = CGPoint(x: randomX, y: randomY)
+    }
+    
+    func randomizeImageSize(){
+        let randomSize = CGFloat.random(in: 100...300)
+        self.imageSize = CGSize(width: randomSize, height: randomSize)
+    }
+}
+
+func chooseRandomImage() -> String {
+    let array = images
+    // an optional is a var or constant that can hold a value or no value
+    // ! means an optional is being unwrapped
+    let result = array.randomElement()!
+    return result
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+
---
+++ Week03/Week03Ramdomize/Week03Ramdomize/Week03RamdomizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Week03RamdomizeApp.swift
+//  Week03Ramdomize
+//
+//  Created by Kaitlyn Zou on 9/28/23.
+//
+
+import SwiftUI
+
+@main
+struct Week03RamdomizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
