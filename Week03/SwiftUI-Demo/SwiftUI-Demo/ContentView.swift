0dbacaac90cb1af16f86c460c3461473d9842cdc
kaitlynzou <kaitlyn.zou@gmail.com>
2023-09-27 22:00:27 -0400

    Week03 single view randomize

---
+++ Week03/SwiftUI-Demo/SwiftUI-Demo/ContentView.swift
@@ -0,0 +1,27 @@
+//
+//  ContentView.swift
+//  SwiftUI-Demo
+//
+//  Created by Kaitlyn Zou on 9/21/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+                .font(.largeTitle)
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Week03/SwiftUI-Demo/SwiftUI-DemoUITests/SwiftUI_DemoUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  SwiftUI_DemoUITestsLaunchTests.swift
+//  SwiftUI-DemoUITests
+//
+//  Created by Kaitlyn Zou on 9/21/23.
+//
+
+import XCTest
+
+final class SwiftUI_DemoUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
