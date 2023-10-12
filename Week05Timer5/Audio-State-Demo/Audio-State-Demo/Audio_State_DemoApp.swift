635736a4d64f4c5554e6215df12dad694d31c9fd
zoukaitlyn <121917589+zoukaitlyn@users.noreply.github.com>
2023-10-12 00:22:05 -0400

    Week 05

---
+++ Week05Timer5/Audio-State-Demo/Audio-State-Demo/Audio_State_DemoApp.swift
@@ -0,0 +1,20 @@
+//
+//  Audio_State_DemoApp.swift
+//  Audio-State-Demo
+//
+//  Created by jht2 on 9/14/23.
+//
+
+import SwiftUI
+
+@main
+struct Audio_State_DemoApp: App {
+//    @StateObject var audioDJ = AudioDJ()
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+//                .environmentObject(audioDJ)
+        }
+    }
+}
---
+++ Week05Timer5/README.md
@@ -0,0 +1,8 @@
+# [04-Audio-State-Demo](https://github.com/mobilelabclass-itp/04-Audio-State-Demo)
+
+- Demonstration of using AVAudioPlayer to play both internal and web based audio files
+
+- Tracking data using @State, @StateObject and @EnvironmentObject
+
+- NavigationLink to create random navigation while audio continues to play
+
